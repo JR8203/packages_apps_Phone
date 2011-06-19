83247a2375aed89b11762fc0c4a47896013f46b7
xoomdev <xoomdevnet@gmail.com>
2011-06-19 18:57:10 -0500

    changes for compile

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -531,11 +531,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         return true;
     }
 
-    public void setRilPowerOff() {
-        enforceModifyPermission();
-        mPhone.setRilPowerOff();
-    }
-
     public boolean enableDataConnectivity() {
         enforceModifyPermission();
         return mPhone.enableDataConnectivity();
