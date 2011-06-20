45cbc3d36a9854c5e64e7d0609d9b81984ca87ee
xoomdev <xoomdevnet@gmail.com>
2011-06-20 07:56:01 -0500

    Build fixes

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -648,11 +648,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         return true;
     }
 
-    public void setRilPowerOff() {
-        enforceModifyPermission();
-        mPhone.setRilPowerOff();
-    }
-
     public boolean enableDataConnectivity() {
         enforceModifyPermission();
         return getPhone(PhoneApp.getDataSubscription()).enableDataConnectivity();
